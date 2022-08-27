5c94d777e8f8652cc53cb0878ae577562be096ce
Ali Amjad <66186390+Ali-A-Koye@users.noreply.github.com>
2022-08-27 11:58:06 +0300

    Demo in Place

---
+++ demo/dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const port = 3000;
+app.get('/', (req, res) => {
+    res.send('Express + TypeScript Server');
+});
+app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
