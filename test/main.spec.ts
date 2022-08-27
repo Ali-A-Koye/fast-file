24d6eb32036fa9295bca15d1c5dde53d347a9411
Ali Amjad <66186390+Ali-A-Koye@users.noreply.github.com>
2022-08-27 22:45:42 +0300

    Adding Integration Tests

--- test/main.spec.ts
+++ test/main.spec.ts
@@ -1,18 +1,32 @@
-import request from 'supertest';
-import server from '../demo/src/index';
+import request from "supertest";
+import server from "../demo/src/index";
 
-describe('Get Test Endpoint', () => {
-  afterAll(done => {
+describe("Get Test Endpoint", () => {
+  afterAll((done) => {
     server.close();
-    done()
-  })
-  it('First Test', async () => {
-    const res = await request(server).get('/').expect(200);
-  })
-  it('pdf file test with 3 parameters', async () => {
-    const res = await request(server).get('/pdf') 
-    .expect(200)
-    .responseType('blob');
-  })
+    done();
+  });
+  it("First Test", async () => {
+    const res = await request(server).get("/").expect(200);
+  });
+  it("pdf file test with 3 parameters", async () => {
+    const res = await request(server)
+      .get("/pdf")
+      .expect(200)
+      .responseType("blob");
+  });
 
-})
+  it("excel file test with 3 parameters", async () => {
+    const res = await request(server)
+      .get("/excel")
+      .expect(200)
+      .responseType("blob");
+  });
+
+  it("docx file test with 3 parameters", async () => {
+    const res = await request(server)
+      .get("/docx")
+      .expect(200)
+      .responseType("blob");
+  });
+});
