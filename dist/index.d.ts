88a3487dba0459ab0c0ca20cecd3f1e3861993af
Ali Amjad <66186390+Ali-A-Koye@users.noreply.github.com>
2022-08-27 12:58:02 +0300

    fixing the TS error

--- dist/index.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function findEmojis(str: string): string;
--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.findEmojis = void 0;
-function findEmojis(str) {
-    return "Hello World";
-}
-exports.findEmojis = findEmojis;
